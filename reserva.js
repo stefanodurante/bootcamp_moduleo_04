298364fbe294a98b7c7e6fc2a821e438a63d2da9
stefano durante <duranteorama@gmail.com>
2019-10-24 16:06:33 +0200

    modulo 04

--- reserva.js
+++ reserva.js
@@ -1,7 +1,15 @@
-var getRoomType = () => document.getElementById("room-type").value; 
-var getSpa = () => document.getElementById("spa").value; 
-var getRoomCapacity = () => document.getElementById("room-capacity").value; 
+var getRoomType = () => document.getElementById("room-type").value;
+var getSpa = () => document.getElementById("spa").checked;
+var getRoomCapacity = () => document.getElementById("room-capacity").value;
+var nightsNum = () => document.getElementById("nights").value;
+var nightsParkNum = () => document.getElementById("park-nights").value;
 
+function handleInfo() {
+  console.log(getRoomType());
+  console.log(getSpa());
+  console.log(getRoomCapacity());
+  console.log(nightsNum());
+  console.log(nightsParkNum());
+}
 
-console.log(getRoomType());
-console.log(getSpa());
+document.getElementById("calculate").addEventListener("click", handleInfo); 
